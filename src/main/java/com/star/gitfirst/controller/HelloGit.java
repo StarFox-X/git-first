24ba44a206aeb3c8ea4658582a35825056b2bd64
StarX <96906098+StarFox-X@users.noreply.github.com>
2023-02-11 15:17:09 +0800

    pull test commit

--- src/main/java/com/star/gitfirst/controller/HelloGit.java
+++ src/main/java/com/star/gitfirst/controller/HelloGit.java
@@ -8,5 +8,6 @@ public class HelloGit {
         System.out.println("Hello Git!");
         System.out.println("Hello Git4");
         System.out.println("push Hello Git4");
+        System.out.println("pull Hello Git4");
     }
 }
