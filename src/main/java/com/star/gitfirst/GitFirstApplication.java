5f59715d6a9325dfa7e83e809bd44a1101fea127
star <xing-star@foxmail.com>
2023-02-11 14:39:49 +0800

    第一次提交

---
+++ src/main/java/com/star/gitfirst/GitFirstApplication.java
@@ -0,0 +1,13 @@
+package com.star.gitfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitFirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/star/gitfirst/GitFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.gitfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
